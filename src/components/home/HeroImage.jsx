7e408e9af8d91bd3ba3de856d7fc30057f00d656
Cehmenang <heaven.cehwin12@gmail.com>
2024-06-13 17:53:03 +0700

    Halaman home inisiasi 7

--- src/components/home/HeroImage.jsx
+++ src/components/home/HeroImage.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function HeroImage(){
     return(
        <div className="hero border-2 border-dark w-full">
-            <Image src={'./img/hero.jpg'} layout="full"/>
+            <Image src={'/img/hero.jpg'} layout="full"/>
        </div>
     )
 }
--- src/components/navbar/LogoNav.jsx
+++ src/components/navbar/LogoNav.jsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function LogoNav(){
     return(
         <Link className="logo-nav flex items-center gap-x-2" href={'/'}>
-            <Image src={'./img/logo.png'} width={50} height={30} className="relative top-[-3px]"/>
+            <Image src={'/img/logo.png'} width={50} height={30} className="relative top-[-3px]"/>
             <h1 className="text-[24px] font-semibold">Annisa Interior</h1>
         </Link>
     )
