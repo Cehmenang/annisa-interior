889bee6678a80e9146fba234e45e8f8ffc83aeab
Cehmenang <heaven.cehwin12@gmail.com>
2024-06-26 17:48:46 +0700

    Carousel Beta

--- src/components/home/HeroImage.jsx
+++ src/components/home/HeroImage.jsx
@@ -1,9 +1,16 @@
-import Image from "next/image"
+import Carousel from "./Carousel"
 
 export default function HeroImage(){
+    const images = [
+        { url: '/img/hero.jpg' },
+        { url: '/img/hero2.jpg' },
+        { url: '/img/hero3.jpg' },
+    ]
     return(
-       <div className="hero border-2 border-dark w-full">
-            <Image src={'/img/hero.jpg'} layout="full"/>
-       </div>
+        <div className="outer-hero">
+            <div className="hero border-2 border-dark w-full">
+                <Carousel images={images}/>
+            </div>
+        </div>
     )
 }
--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -3,7 +3,7 @@ import MenuNav from "./MenuNav"
 
 export default function Nav(){
     return (
-        <nav className="w-full bg-third text-light absolute">
+        <nav className="w-full bg-third text-light absolute z-10">
             <div className="navbar w-[90%] mx-auto flex justify-between py-3 items-center">
                 <LogoNav/>
                 <MenuNav/>
