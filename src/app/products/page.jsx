09d5cae98b7e6cb298ecccd8418b38ad718a110b
Cehmenang <heaven.cehwin12@gmail.com>
2024-07-23 17:55:51 +0700

    produks menu

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -11,15 +11,15 @@ export default function Products(){
     ]
 
     const showProductMenus = products.map((product, index)=>{
-        return <button key={index} className="px-3 border-[1px] border-dark rounded-2xl">{product.name}</button> 
+        return <button key={index} className="px-4 border-[2px] border-dark rounded-2xl md:w-[140px] text-[14px]">{product.name}</button> 
     })
 
     return (
         <>
         <div className="base pt-20">
-            <h1 className="text-[40px] font-bold">Products</h1>
+            <h1 className="text-[40px] font-bold text-center md:text-left">Products</h1>
         </div>
-        <div className="product-menu flex gap-x-3">
+        <div className="product-menu flex gap-x-3 w-full px-auto flex-wrap justify-center md:justify-normal gap-y-2 md:gap-y-0">
             {showProductMenus}
         </div>
         </>
